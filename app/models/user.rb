f8a933b7faf51ace01cdb4ee1b02916b801b59b9
benjaminmweribaya <b3njaminbaya@gmail.com>
2025-02-13 01:21:38 +0300

    feat: Add authentication with has_secure_password
    - Added has_secure_password to User model for authentication.
    - Implemented validations for email (presence & uniqueness).
    - Added password validation (minimum 6 characters).

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :borrowings, dependent: :destroy
   has_many :books, through: :borrowings
+
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 end
